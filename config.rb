01fd7e15b3efd5b508d4c683a4bd518b3fb671e9
Jordan Johnson <josamjohn@gmail.com>
2013-08-02 23:35:21 -0400

    Major CSS updates, Nav reworked, javascript in multiple files (not complete), still need to finish show individual page and other pages

--- config.rb
+++ config.rb
@@ -4,6 +4,13 @@ set :images_dir, 'img'
 
 activate :livereload
 
+helpers do
+  def nav_active(page)
+    @page_id == page ? {:class => "active"} : {}
+  end
+end
+
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
